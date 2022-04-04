00494b12a4a72dbef696049fe5cffebf1d86d46d
Mahfod <mahfod.dev@gmail.com>
2022-04-04 18:02:28 +0200

    add firebase

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './routes/home/Home'
 import Navigation from './routes/navigation/Navigation'
-import Signin from './routes/signin/Signin'
+import Authentication from './routes/authentication/authentication.component'
 
 const Shop = () => {
 	return <h3>Shop</h3>
@@ -14,7 +14,7 @@ const App = () => {
 			<Route path='/' element={<Navigation />}>
 				<Route index element={<Home />} />
 				<Route path='shop' element={<Shop />} />
-				<Route path='sign-in' element={<Signin />} />
+				<Route path='auth' element={<Authentication />} />
 			</Route>
 		</Routes>
 	)
--- src/routes/navigation/Navigation.jsx
+++ src/routes/navigation/Navigation.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Outlet, Link } from 'react-router-dom'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import './navigation.styles.scss'
+import { UserContext } from '../../context/userContext'
+import { signOutUser } from '../../utils/firebase/Firebase.utils'
 
 const Navigation = () => {
+	const { currentUser, setCurrentUser } = useContext(UserContext)
+
 	return (
 		<>
 			<nav className='navigation'>
@@ -17,9 +21,15 @@ const Navigation = () => {
 					<Link className='nav-link' to='/shop'>
 						SHOP
 					</Link>
-					<Link className='nav-link' to='/sign-in'>
-						SIGNIN
-					</Link>
+					{currentUser ? (
+						<span className='nav-link' onClick={signOutUser}>
+							SIGN OUT
+						</span>
+					) : (
+						<Link className='nav-link' to='/auth'>
+							SIGNIN
+						</Link>
+					)}
 				</div>
 			</nav>
 
